0407d2e1c8d552703bb5af371f02be579f4c9814
yjernite <jernite@cs.nyu.edu>
2015-08-26 15:32:51 -0400

    done

--- UMLS/ProcessUMLS.py
+++ UMLS/ProcessUMLS.py
@@ -3,139 +3,162 @@
 
 import string
 import pickle
+import argparse
+import glob
+import sys
+import os
+from os.path import join as pjoin
 
-f = open('MRCONSO.RRF')
-mrconso = f.readlines()
-f.close()
-mrconso = map(lambda x: x.split('|'), mrconso)
-foo = map(aux, mrconso)
-
-concepts = {}
-strtoid = {}
-nametoid = {}
-cuilist = []
-
-index = 0
-
-for (i, ls) in enumerate(mrconso):
-    if i % 10000 == 0:
-        print i
-    if not ls[1] == 'ENG':
-        continue    # only treat english language concepts
-    cui = ls[0]
-    st = ls[14].lower()
-    if cui not in concepts:
-        concepts[cui] = {}
-        concepts[cui]['id'] = index
-        index += 1
-        cuilist += [cui]
-        concepts[cui]['cui'] = cui
-        concepts[cui]['strings'] = []
-        concepts[cui]['parents'] = []
-        concepts[cui]['children'] = []
-        concepts[cui]['broader'] = []
-        concepts[cui]['narrower'] = []
-        concepts[cui]['qualifies'] = []
-        concepts[cui]['qualified'] = []
-        concepts[cui]['synonyms'] = []
-        concepts[cui]['types'] = []
-    if ls[2] == 'P' and ls[4] == 'PF':
-            concepts[cui]['name'] = st
-            nametoid[st] = concepts[cui]['id']
-    if st not in concepts[cui]['strings']:
-        concepts[cui]['strings'] += [st]
-        strtoid[st] = concepts[cui]['id']
-
-pprint(concepts[cuilist[strtoid['fevers']]])
-
-f = open('MRREL.SY.RRF')
-syns = f.readlines()
-f.close()
-
-syns = map(lambda x: x.split('|'), syns)
-syns = map(lambda x: (x[0], x[4]), syns)
-syns = filter(lambda x: not (x[0] == x[1]), syns)
-
-for (a, b) in syns:
-    if a in concepts and b in concepts:
-        concepts[a]['synonyms'] += [concepts[b]['id']]
-
-f = open('MRREL.PAR.RRF')
-syns = f.readlines()
-f.close()
-
-syns = map(lambda x: x.split('|'), syns)
-syns = map(lambda x: (x[0], x[4]), syns)
-syns = filter(lambda x: not (x[0] == x[1]), syns)
-syns = list(set(syns))
-
-for (a, b) in syns:
-    if a in concepts and b in concepts:
-        concepts[a]['parents'] += [concepts[b]['id']]
-        concepts[b]['children'] += [concepts[a]['id']]
+write_pickle = False
+pickle_file = 'pyUMLS.pk'
+write_text = False
+text_file = 'UMLSlite.dat'
+write_rels = False
+METAdir = ''
 
 
-f = open('MRREL.AQ.RRF')
-syns = f.readlines()
-f.close()
-
-syns = map(lambda x: x.split('|'), syns)
-syns = map(lambda x: (x[0], x[4]), syns)
-syns = filter(lambda x: not (x[0] == x[1]), syns)
-syns = list(set(syns))
-
-for (a, b) in syns:
-    if a in concepts and b in concepts:
-        concepts[a]['qualified'] += [concepts[b]['id']]
-        concepts[b]['qualifies'] += [concepts[a]['id']]
-
-
-f = open('MRREL.RB.RRF')
-syns = f.readlines()
-f.close()
-
-syns = map(lambda x: x.split('|'), syns)
-syns = map(lambda x: (x[0], x[4]), syns)
-syns = filter(lambda x: not (x[0] == x[1]), syns)
-syns = list(set(syns))
-
-for (a, b) in syns:
-    if a in concepts and b in concepts:
-        concepts[a]['broader'] += [concepts[b]['id']]
-        concepts[b]['narrower'] += [concepts[a]['id']]
-
-f = open('MRSTY.RRF')
-sty = f.readlines()
-f.close()
-
-for li in sty:
-    ls = li.split('|')
-    cui = ls[0]
-    ty = ls[3]
-    if cui in concepts and ty not in concepts[cui]['types']:
-        concepts[cui]['types'] += [ty]
-
-pickle.dump([concepts, cuilist, strtoid, nametoid], open('pyUMLS.k', 'w'))
-
-f = open('UMLSlite.dat', 'w')
-
-keyset = ['cui', 'name', 'id', 'types', 'strings', 'narrower', 'broader',
-          'synonyms', 'qualifies', 'qualified', 'children', 'parents']
-
-keyset = keyset[:5]
-for k in keyset:
-    print >>f, k, '||||',
-
-print >>f, ''
-
-ct = 0.
-for cu in cuilist:
-    if ct % 50000 == 0:
-        print (100*ct)/len(cuilist)
-    for k in keyset[:8]:
-        print >>f, concepts[cu][k], '||||',
-    print>>f, ''
-    ct += 1
+def read_concepts(METAdir):
+    i = 0
+    index = 0
+    concepts = {}
+    strtoid = {}
+    nametoid = {}
+    cuilist = []
+    f = open(pjoin(METAdir, 'MRCONSO.RRF'))
+    for line in f:
+        if i % 100000 == 0:
+            print 'concepts:', i, 'lines read'
+        i += 1
+        ls = line.strip().split('|')
+        if not ls[1] == 'ENG':
+            continue    # only treat english language concepts
+        cui = ls[0]
+        st = ls[14].lower()
+        if cui not in concepts:
+            concepts[cui] = {}
+            concepts[cui]['id'] = index
+            index += 1
+            cuilist += [cui]
+            concepts[cui]['cui'] = cui
+            concepts[cui]['strings'] = []
+            concepts[cui]['parents'] = []
+            concepts[cui]['children'] = []
+            concepts[cui]['broader'] = []
+            concepts[cui]['narrower'] = []
+            concepts[cui]['qualifies'] = []
+            concepts[cui]['qualified'] = []
+            concepts[cui]['types'] = []
+        if ls[2] == 'P' and ls[4] == 'PF':
+                concepts[cui]['name'] = st
+                nametoid[st] = concepts[cui]['id']
+        if st not in concepts[cui]['strings']:
+            concepts[cui]['strings'] += [st]
+            strtoid[st] = concepts[cui]['id']
+    f.close()
+    return concepts, strtoid, nametoid, cuilist
 
 
-f.close()
+def read_relations(METAdir, concepts):
+    i = 0
+    f = open(pjoin(METAdir, 'MRREL.RRF'))
+    for line in f:
+        if i % 100000 == 0:
+            print 'relations:', i, 'lines read'
+        i += 1
+        ls = line.strip().split('|')
+        (a, b) = (ls[0], ls[4])
+        rel_type = ls[3]
+        if rel_type == 'PAR':
+            if a in concepts and b in concepts:
+                concepts[a]['parents'] += [concepts[b]['id']]
+                concepts[b]['children'] += [concepts[a]['id']]
+        elif rel_type == 'AQ':
+            if a in concepts and b in concepts:
+                concepts[a]['qualified'] += [concepts[b]['id']]
+                concepts[b]['qualifies'] += [concepts[a]['id']]
+        elif rel_type == 'RB':
+            if a in concepts and b in concepts:
+                concepts[a]['broader'] += [concepts[b]['id']]
+                concepts[b]['narrower'] += [concepts[a]['id']]
+    f.close()
+
+
+def read_sem_types(METAdir, concepts):
+    i = 0
+    f = open(pjoin(METAdir, 'MRSTY.RRF'))
+    for line in f:
+        if i % 100000 == 0:
+            print 'types:', i, 'lines read'
+        i += 1
+        ls = line.split('|')
+        (cui, ty) = (ls[0], ls[3])
+        if cui in concepts and ty not in concepts[cui]['types']:
+            concepts[cui]['types'] += [ty]
+    f.close()
+
+
+def write_text_umls():
+    global text_file
+    global write_rels
+    f = open(text_file, 'w')
+    keyset = ['cui', 'name', 'id', 'types', 'strings', 'narrower', 'broader',
+              'qualifies', 'qualified', 'children', 'parents']
+    if not write_rels:
+        keyset = keyset[:5]
+    for k in keyset:
+        print >>f, k, '||||',
+    print >>f, ''
+    ct = 0.
+    for cu in cuilist:
+        if ct % 50000 == 0:
+            print (100*ct)/len(cuilist), 'written'
+        for k in keyset[:8]:
+            print >>f, concepts[cu][k], '||||',
+        print>>f, ''
+        ct += 1
+    f.close()
+
+
+def main():
+    global METAdir
+    global write_pickle
+    global write_text
+    global pickle_file
+    concepts, strtoid, nametoid, cuilist = read_concepts(METAdir)
+    read_relations(METAdir, concepts)
+    read_sem_types(METAdir, concepts)
+    if write_pickle:
+        pickle.dump([concepts, cuilist, strtoid, nametoid],
+                    open(pickle_file, 'w'))
+    if write_text:
+        write_text_umls()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='This program takes some \
+     of the RRF files output by the Metamorphosys subset creator, and parses \
+     them into a lighter text or pickle format')
+    parser.add_argument("-data", "--corenlp",
+                        help="location of the UMLS installation directory \
+                              (hint: should contain a META and a NET folder)")
+    parser.add_argument("-po", "--pickle_out",
+                        help="pickle format output (optional)")
+    parser.add_argument("-o", "--text_out",
+                        help="text format output")
+    parser.add_argument("-rels", "--write_rels",
+                        help="writes relations (qualifies, broader than, \
+                         etc...) in the text output", action="store_true")
+    args = parser.parse_args()
+    if args.data:
+        METAdir = os.path.abspath(pjoin(args.data, 'META'))
+    if args.pickle_out:
+        pickle_file = os.path.abspath(args.pickle_out)
+        write_pickle = True
+    if args.text_out:
+        text_file = os.path.abspath(args.text_out)
+        write_text = True
+    if args.write_rels:
+        write_rels = True
+    assert write_pickle or write_text, 'You are not saving anything'
+    print 'Starting'
+    main()
